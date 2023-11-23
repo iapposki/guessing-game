7a8bec43b1bc10208e5b42e062367f03fe458b59
iapposki <konark981999@gmail.com>
2023-11-23 17:27:04 +0530

    getting started : cargo common commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// cargo new : create new project
+// cargo build : makes an executable in target/debug/<name_of_project>.<extension> (extension depending on the os : exe or no extension in linux)
+// cargo check : check if the code compiles or not
+// cargo build --release : to compile it with optimizations
