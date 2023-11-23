0efc8ecafd14c904130168014313a4faaed0e034
iapposki <konark981999@gmail.com>
2023-11-23 17:29:24 +0530

    getting started

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
 fn main() {
     println!("Hello, world!");
 }
-
-// cargo new : create new project
-// cargo build : makes an executable in target/debug/<name_of_project>.<extension> (extension depending on the os : exe or no extension in linux)
-// cargo check : check if the code compiles or not
-// cargo build --release : to compile it with optimizations
